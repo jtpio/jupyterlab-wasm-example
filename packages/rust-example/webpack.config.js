4aa9545894b149820ccf4155e72aedf9aab118d6
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2020-10-17 14:59:41 +0200

    Add rust package and webpack 5

---
+++ packages/rust-example/webpack.config.js
@@ -0,0 +1,31 @@
+const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
+const WasmPackPlugin = require("@wasm-tool/wasm-pack-plugin");
+
+const dist = path.resolve(__dirname, "dist");
+
+module.exports = {
+  mode: "production",
+  entry: {
+    index: "./js/index.js"
+  },
+  output: {
+    path: dist,
+    filename: "[name].js"
+  },
+  experiments: {
+    asyncWebAssembly: true
+  },
+  devServer: {
+    contentBase: dist,
+  },
+  plugins: [
+    new CopyPlugin([
+      path.resolve(__dirname, "static")
+    ]),
+
+    new WasmPackPlugin({
+      crateDirectory: __dirname,
+    }),
+  ]
+};
