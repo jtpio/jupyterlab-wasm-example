93dfdc29941e0b74f89449c45f3ac8bc01a11b5c
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2020-10-17 23:16:40 +0200

    Draw on canvas

--- packages/jupyterlab-wasm-example/src/index.ts
+++ packages/jupyterlab-wasm-example/src/index.ts
@@ -3,14 +3,49 @@ import {
   JupyterFrontEndPlugin
 } from '@jupyterlab/application';
 
+import { ICommandPalette } from '@jupyterlab/apputils';
+
+const { add, open_canvas } = await import('../../rust-example/pkg');
+
+namespace CommandIds {
+  export const createCanvas = 'jupyterlab-wasm-example:createCanvas';
+  export const clearCanvas = 'jupyterlab-wasm-example:clearCanvas';
+}
+
 /**
  * Initialization data for the jupyterlab-wasm-example extension.
  */
 const extension: JupyterFrontEndPlugin<void> = {
   id: 'jupyterlab-wasm-example',
   autoStart: true,
-  activate: (app: JupyterFrontEnd) => {
+  optional: [ICommandPalette],
+  activate: async (app: JupyterFrontEnd, palette: ICommandPalette) => {
+    console.log('Calling add(2, 3) from Rust');
+    const res = add(2, 3);
+    console.log(res);
     console.log('JupyterLab extension jupyterlab-wasm-example is activated!');
+
+    const { commands } = app;
+    commands.addCommand(CommandIds.createCanvas, {
+      label: 'Create the Rust Canvas',
+      execute: () => {
+        open_canvas();
+      }
+    });
+
+    commands.addCommand(CommandIds.clearCanvas, {
+      label: 'Clear the Rust Canvas',
+      execute: () => {
+        const canvas = document.getElementById('rust-canvas');
+        canvas?.remove();
+      }
+    });
+
+    if (palette) {
+      [CommandIds.createCanvas, CommandIds.clearCanvas].forEach(command => {
+      palette.addItem({ command, category: 'WebAssembly' });
+      })
+    }
   }
 };
 
--- packages/rust-example/src/lib.rs
+++ packages/rust-example/src/lib.rs
@@ -1,6 +1,8 @@
 use wasm_bindgen::prelude::*;
+use wasm_bindgen::JsCast;
 use web_sys::console;
-
+use std::rc::Rc;
+use std::cell::Cell;
 
 // When the `wee_alloc` feature is enabled, this uses `wee_alloc` as the global
 // allocator.
@@ -11,23 +13,86 @@ use web_sys::console;
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 
-// This is like the `main` function, except for JavaScript.
+#[wasm_bindgen]
+pub fn add(a: u32, b: u32) -> u32 {
+    let res = a + b;
+    res
+}
+
+// Called by our JS entry point to run the example
 #[wasm_bindgen(start)]
-pub fn main_js() -> Result<(), JsValue> {
+pub fn run() -> Result<(), JsValue> {
     // This provides better error messages in debug mode.
     // It's disabled in release mode so it doesn't bloat up the file size.
     #[cfg(debug_assertions)]
     console_error_panic_hook::set_once();
 
-
     // Your code goes here!
-    console::log_1(&JsValue::from_str("Hello JupyterLab!"));
+    console::log_1(&JsValue::from_str("Hello JupyterLab, from Rust!"));
 
     Ok(())
 }
 
+
 #[wasm_bindgen]
-pub fn add(a: u32, b: u32) -> u32 {
-    let res = a + b;
-    res
+pub fn open_canvas() -> Result<(), JsValue> {
+    let document = web_sys::window().unwrap().document().unwrap();
+    let canvas = document
+        .create_element("canvas")?
+        .dyn_into::<web_sys::HtmlCanvasElement>()?;
+    document.body().unwrap().append_child(&canvas)?;
+    canvas.set_id("rust-canvas");
+
+    let w = web_sys::window().unwrap().inner_width().unwrap().as_f64().unwrap() as u32;
+    let h = web_sys::window().unwrap().inner_height().unwrap().as_f64().unwrap() as u32;
+    canvas.set_width(w);
+    canvas.set_height(h);
+    canvas.style().set_property("border", "solid")?;
+    canvas.style().set_property("z-index", "9999999")?;
+    canvas.style().set_property("position", "absolute")?;
+    let context = canvas
+        .get_context("2d")?
+        .unwrap()
+        .dyn_into::<web_sys::CanvasRenderingContext2d>()?;
+    let context = Rc::new(context);
+    let pressed = Rc::new(Cell::new(false));
+    {
+        let context = context.clone();
+        let pressed = pressed.clone();
+        let closure = Closure::wrap(Box::new(move |event: web_sys::MouseEvent| {
+            context.begin_path();
+            context.move_to(event.offset_x() as f64, event.offset_y() as f64);
+            pressed.set(true);
+        }) as Box<dyn FnMut(_)>);
+        canvas.add_event_listener_with_callback("mousedown", closure.as_ref().unchecked_ref())?;
+        closure.forget();
+    }
+    {
+        let context = context.clone();
+        let pressed = pressed.clone();
+        let closure = Closure::wrap(Box::new(move |event: web_sys::MouseEvent| {
+            if pressed.get() {
+                context.line_to(event.offset_x() as f64, event.offset_y() as f64);
+                context.set_line_width(10.0);
+                context.stroke();
+                context.begin_path();
+                context.move_to(event.offset_x() as f64, event.offset_y() as f64);
+            }
+        }) as Box<dyn FnMut(_)>);
+        canvas.add_event_listener_with_callback("mousemove", closure.as_ref().unchecked_ref())?;
+        closure.forget();
+    }
+    {
+        let context = context.clone();
+        let pressed = pressed.clone();
+        let closure = Closure::wrap(Box::new(move |event: web_sys::MouseEvent| {
+            pressed.set(false);
+            context.line_to(event.offset_x() as f64, event.offset_y() as f64);
+            context.stroke();
+        }) as Box<dyn FnMut(_)>);
+        canvas.add_event_listener_with_callback("mouseup", closure.as_ref().unchecked_ref())?;
+        closure.forget();
+    }
+
+    Ok(())
 }
